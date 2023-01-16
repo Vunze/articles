dfd7d4bf868d57c30d0d236561c8f1fa4567e769
Kirill Nebroev <kvnebroev@edu.hse.ru>
2023-01-16 20:27:20 +0300

    Privacy fix

--- upload_to_db.py
+++ upload_to_db.py
@@ -30,7 +30,7 @@ class Article:
 
 def get_database():
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb+srv://vunz:fuloron67@articles.zavth9x.mongodb.net/?retryWrites=true&w=majority"
+    CONNECTION_STRING = # process.env.DB_CONNECT
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(CONNECTION_STRING)
