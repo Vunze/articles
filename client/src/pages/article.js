e37c9c1a085a61e822991ed61db6556d41103b2b
Kirill Nebroev <kvnebroev@edu.hse.ru>
2023-07-25 13:57:43 +0300

    modifications to article.js and articleRef.js

--- client/src/pages/article.js
+++ client/src/pages/article.js
@@ -4,15 +4,46 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import formatDate from '../context/formatDate';
 import axios from "axios";
+import Button from 'react-bootstrap/Button';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Stack from 'react-bootstrap/Stack';
+import Form from "react-bootstrap/Form";
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Article = () => {
   const { id: articleId} = useParams();
   const [article, setArticle] = useState({});
+  const [show, setShow] = useState(false);
+  const [searchInput, setSearchInput] = useState("")
+
+
+  const handleClose = () => {
+    setShow(false)
+    setSearchInput("")
+  };
+  const handleShow = () => setShow(true);
+
+  const renderTooltipArticle = (props) => (
+    <Tooltip id="article-tooltip" {...props}>
+      Article rating is the sum of citations and references
+    </Tooltip>
+  );
+
+  const renderTooltipAuthor = (props) => (
+    <Tooltip id="author-tooltip" {...props}>
+      Author rating is the average of H-indices of all authors
+    </Tooltip>
+  );
 
   useEffect(() => {
     async function getData() {
       console.log(articleId)
       const { data } = await axios.get(`http://meatael.pythonanywhere.com/api/articles/${articleId}`);
+      console.log(data);
       setArticle(data);
     }
     try {
@@ -37,35 +68,79 @@ const Article = () => {
     let links1 = links.split(", ")
     return links1.filter(a => a !== pdf_link(links))
   }
-
+  
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value)
+}
 
   return (
       <>
-        <Container className="my-5 text-justified" style={{ maxWidth: '800px' }}>
+        <Container className="my-4 text-justified" style={{ maxWidth: '800px' }}>
           <h1><a href={article.links && pdf_link(article.links)}>{article.title}</a></h1>
+          <div className="text-secondary mb-1">{article.conference ? "Presented on " + article.conference : ""}</div>
           <div className="text-secondary mb-1">{article.published && formatDate(article.published)}</div>
-          <div className="text-secondary mb-4">{"Article rating - " + article.articleRating + ". Author rating - " + article.authorRating}</div>
-          <div className='text-secondary mb-2'>{article.summary && article.summary}</div>
+          <Row>
+            <Col>
+              <OverlayTrigger
+                placement='left'
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltipArticle}
+              >
+                <div className="text-secondary mb-4">{"Article rating - " + article.articleRating}</div>
+              </OverlayTrigger>
+            </Col>
+            <Col>
+              <OverlayTrigger
+                placement='left'
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltipAuthor}
+              >
+                <div className="text-secondary mb-4" style={{display: "flex", alignItems: "end"}}>{"Author rating - " + article.authorRating}</div>
+              </OverlayTrigger>
+            </Col>
+          </Row>
+          <div className='text-secondary mb-2' style={{display: "flex", alignItems: "center", margin: 5}}>{article.summary && article.summary}</div>
           <div className="text-secondary mb-5">- {article.authors}</div>
-          <div className='text-secondary mb-6'>References</div>
-          <ListGroup variant="flush" as="ol">
-            {article.references && article.references.map((reference) => {
-              if (reference.paperId)
-                return (
-                    <ListGroup.Item key={reference.paperId}>
-                      <div>
-                        {reference.paperId ? <Link to={`/articles/${reference.paperId}/${reference.title}`} style={{ textDecoration: 'none' }}>{reference.title}</Link> : ""}
-                      </div>
-                    </ListGroup.Item>
-                )
-            })}
-          </ListGroup>
-          <div className="text-secondary mb-6">{article.links && (not_pdf_links(article.links) ? "Other Links" : "")}</div>
-          <div className="text-secondary mb-6">{article.links &&
-          not_pdf_links(article.links).map((link) => {
-            return <a href={link}>{link}</a>
-          })}</div>
-          <Link to="/" style={{ textDecoration: 'none' }}>Back to Home</Link>
+          <Row>
+            <Button variant="light" onClick={handleShow} style={{alignItems: "center"}}>
+            Citations
+            </Button>
+            <Offcanvas show={show} onHide={handleClose} placement='bottom' scroll="true">
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Articles that have cited this document</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Form.Control type="text"
+                                  placeholder='Search by title'
+                                  onChange={handleSearch}
+                                  value={searchInput}/>
+              <ListGroup style={{overflowY: 'scroll'}}>
+                {article.citations && article.citations.filter((article) => {
+                        return article.title.toLowerCase().match(searchInput.toLowerCase())
+                    }).map((citation) => {
+                      console.log(citation);
+                      return (
+                        <ListGroup.Item key={citation.paperId}>
+                          <div>
+                            {citation.paperId ? <Link to={`/articles/${citation.paperId}/${citation.title}`} style={{ textDecoration: 'none' }}>{citation.title}</Link> : ""}
+                          </div>
+                        </ListGroup.Item>
+                      )
+                })}
+              </ListGroup>
+            </Offcanvas>
+          </Row>
+          <Row>
+            <Stack direction="horizontal" gap={4}>
+              <div className='text-secondary mb-5'>{article.links && (not_pdf_links(article.links) ? "Other Links" : "")}</div>
+              <div className='text-secondary mb-5'>{article.links &&
+                not_pdf_links(article.links).map((link) => {
+                  return <a href={link}>{link}</a>
+                })}
+              </div>
+            </Stack>
+          </Row>
+          <Link to="/" style={{ display: "flex", alignItems: "center", marginTop: 25 }}>Back to Home</Link>
         </Container>
       </>
   );
--- client/src/pages/articleRef.js
+++ client/src/pages/articleRef.js
@@ -3,17 +3,54 @@ import { useParams, useNavigate, Link } from 'react-router-dom';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import formatDate from '../context/formatDate';
+import Button from 'react-bootstrap/Button';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Stack from 'react-bootstrap/Stack';
+import Form from "react-bootstrap/Form";
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 const ArticleCopy = () => {
   const { id: articleId, title: flag} = useParams();
   const [authors, setAuthors] = useState([]);
   const [degreeCentrality, setDegreeCentrality] = useState(0)
   const [authorRating, setAuthorRating] = useState(0)
-  const [references, setReferences] = useState([])
+  const [citations, setCitations] = useState([])
   const [title, setTitle] = useState("")
   const [pubDate, setPubDate] = useState(null)
   const [summary, setSummary] = useState(null)
   const [link, setLink] = useState(null)
+  const [searchInput, setSearchInput] = useState("")
+  const [show, setShow] = useState(false);
+
+
+
+  const handleClose = () => {
+    setShow(false)
+    setSearchInput("")
+  };
+  const handleShow = () => setShow(true);
+  const renderTooltipArticle = (props) => (
+    <Tooltip id="article-tooltip" {...props}>
+      Article rating is the sum of citations and references
+    </Tooltip>
+  );
+
+  const renderTooltipAuthor = (props) => (
+    <Tooltip id="author-tooltip" {...props}>
+      Author rating is the average of H-indices of all authors
+    </Tooltip>
+  );
+  
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value)
+  }
+
   const semanticLink = "https://api.semanticscholar.org/graph/v1/paper/"
         + articleId
         + "?fields=title,publicationDate,url,openAccessPdf,isOpenAccess,citations,references,referenceCount,citationCount,tldr,authors.name,authors.hIndex"
@@ -35,13 +72,14 @@ const ArticleCopy = () => {
     }
     return res;
   }
+  
 
   useEffect(() => {
     async function getData() {
       fetch(semanticLink)
       .then(response => response.json())
       .then(data => {
-        if (data["references"]) setReferences(data["references"])
+        if (data["citations"]) setCitations(data["citations"])
         if (data["authors"]) {
             setAuthors(authorsNames(data['authors']));
             setAuthorRating(calcAuthorRating(data['authors']))
@@ -70,23 +108,59 @@ const ArticleCopy = () => {
       {articleId ? <Container className="my-5 text-justified" style={{ maxWidth: '800px' }}>
         <h1><a href={link}>{title}</a></h1>
         <div className="text-secondary mb-1">{pubDate && formatDate(pubDate)}</div>
-        <div className="text-secondary mb-4">{"Article rating - " + degreeCentrality + ". Author rating - " + authorRating}</div>
-        <div className='text-secondary mb-2'>{summary ? summary : ""}</div>
+        <Row>
+            <Col>
+              <OverlayTrigger
+                placement='right'
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltipArticle}
+                style={{display:"flex"}}
+              >
+                <div className="text-secondary mb-4">{"Article rating - " + (degreeCentrality !== Infinity ? degreeCentrality : "")}</div>
+              </OverlayTrigger>
+            </Col>
+            <Col xs={7}></Col>
+            <Col>
+              <OverlayTrigger
+                placement='left'
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltipAuthor}
+              >
+                <div className="text-secondary mb-4" style={{display: "flex", alignItems: "end"}}>{"Author rating - " + (authorRating !== Infinity ? authorRating : '')}</div>
+              </OverlayTrigger>
+            </Col>
+          </Row>
+        <div className='text-secondary mb-2' style={{display: "flex", alignItems: "center", margin: 5}}>{summary ? summary : ""}</div>
         <div className="text-secondary mb-5">- {authors && authors.join(", ")}</div>
-        {references.length > 0 ? <div className='text-secondary mb-6'>References</div> : ""}
-        <ListGroup variant="flush" as="ol">
-          {references.map((reference) => {
-            if (reference.paperId)
-            return (
-              <ListGroup.Item key={reference.paperId}>
-                <div>
-                    {reference.paperId ? <Link to={`/articles/${reference.paperId}/${reference.title}`} style={{ textDecoration: 'none' }}>{reference.title}</Link> : ""}
-                </div>
-              </ListGroup.Item>
-            )
-          })}
-        </ListGroup>
-        <Link to="/" style={{ textDecoration: 'none' }}>Back to Home</Link>
+        <Row>
+            <Button variant="light" onClick={handleShow} style={{alignItems: "center"}}>
+            Citations
+            </Button>
+            <Offcanvas show={show} onHide={handleClose} placement='bottom' scroll="true">
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Articles that have cited this document</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Form.Control type="text"
+                                  placeholder='Search by title'
+                                  onChange={handleSearch}
+                                  value={searchInput}/>
+              <ListGroup style={{overflowY: 'scroll'}}>
+                {citations && citations.filter((article) => {
+                        return article.title.toLowerCase().match(searchInput.toLowerCase())
+                    }).map((citation) => {
+                      console.log(citation);
+                      return (
+                        <ListGroup.Item key={citation.paperId}>
+                          <div>
+                            {citation.paperId ? <Link to={`/articles/${citation.paperId}/${citation.title}`} style={{ textDecoration: 'none' }}>{citation.title}</Link> : ""}
+                          </div>
+                        </ListGroup.Item>
+                      )
+                })}
+              </ListGroup>
+            </Offcanvas>
+          </Row>
+          <Link to="/" style={{ display: "flex", alignItems: "center", marginTop: 25 }}>Back to Home</Link>
       </Container> : "article not found"}
     </>
   );
